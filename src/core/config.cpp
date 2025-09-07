08327096741f4a0bf22b6cbb5ae8b521c3ef366c
Moussa Mokhtari <me@moussamokhtari.com>
2025-09-07 21:45:01 +0300

    On main: Checkpoint: auto_2025_09_07_21_45_01

--- src/core/config.cpp
+++ src/core/config.cpp
@@ -17,7 +17,7 @@ namespace core {
 
 // Version information
 #ifndef AK_VERSION_STRING
-#define AK_VERSION_STRING "4.1.3"
+#define AK_VERSION_STRING "4.1.5"
 #endif
 const std::string AK_VERSION = AK_VERSION_STRING;
 
