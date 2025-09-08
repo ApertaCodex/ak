9d7f13f9a450959fc1d67f1660491fc31b3ec1a2
Moussa Mokhtari <me@moussamokhtari.com>
2025-09-08 04:25:42 +0300

    🚀 Release v4.2.4

--- src/core/config.cpp
+++ src/core/config.cpp
@@ -17,7 +17,7 @@ namespace core {
 
 // Version information
 #ifndef AK_VERSION_STRING
-#define AK_VERSION_STRING "4.2.3"
+#define AK_VERSION_STRING "4.2.4"
 #endif
 const std::string AK_VERSION = AK_VERSION_STRING;
 
